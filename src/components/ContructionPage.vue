40aef8cb195f198c0ead66896eadc9bccfbbe49a
Kuo Tang <candy_tang@msn.com>
2022-02-02 03:35:52 -0500

    finish construction page, added resume

--- src/components/ContructionPage.vue
+++ src/components/ContructionPage.vue
@@ -1,10 +1,29 @@
 <template>
+  <img src="@/assets/Construction.png" />
   <div class="hello">
-    <p>Hi there! This page is under construction.</p>
+    <p>Hi there! This page is under construction</p>
     <p>
-      Please check out my
-      <a href="https://candytang3.wixsite.com/candytang">old portfolio page</a>
-      in the meantime, thanks : )!
+      In the meantime, please visit my
+      <a href="https://candytang3.wixsite.com/candytang"
+        ><strong> old portfolio page</strong></a
+      >
+    </p>
+    <br /><br />
+    <p>
+      You can also download my
+      <a href="2022 Feb Resume.pdf" target="_blank"><strong> resume</strong></a
+      >,
+    </p>
+    <p>
+      check out my
+      <a href="https://www.linkedin.com/in/candytang92239/" target="_blank"
+        ><strong> LinkedIn page</strong></a
+      >,
+    </p>
+    <p>
+      or give me a shout at
+      <a><strong> candy.tang@mail.utoronto.ca</strong></a
+      >, thank you : )!
     </p>
   </div>
 </template>
@@ -32,6 +51,10 @@ li {
   margin: 0 10px;
 }
 a {
-  color: #42b983;
+  color: #46b1c9;
+}
+img {
+  //padding: 30% 0;
+  max-width: 40%;
 }
 </style>
