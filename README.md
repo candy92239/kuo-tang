2882b11285f0ed7d117c87de9309c43aa68147be
Kuo Tang <candy_tang@msn.com>
2022-02-01 21:35:28 -0500

    construction update

--- README.md
+++ README.md
@@ -1,24 +1,14 @@
 # portfolio
 
-## Project setup
-```
-npm install
-```
-
 ### Compiles and hot-reloads for development
+
 ```
 npm run serve
 ```
 
 ### Compiles and minifies for production
-```
-npm run build
-```
 
-### Lints and fixes files
 ```
-npm run lint
+npm run deploy
+To me: you'll need to enter psw for this, if not, something's wrong
 ```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <ContructionPage />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import ContructionPage from "@/components/ContructionPage.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
+    ContructionPage,
   },
 };
 </script>
