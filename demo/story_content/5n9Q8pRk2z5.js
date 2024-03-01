a3e2210d7ccf8e333baee92ae4a36566411733bc
Kuo Tang <candy_tang@msn.com>
2024-03-01 15:41:52 -0500

    deploy

---
+++ demo/story_content/5n9Q8pRk2z5.js
@@ -0,0 +1,4 @@
+window.globalProvideSvgData(
+  "svg-5n9Q8pRk2z5",
+  '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 9.02 10.58" id="svg-5n9Q8pRk2z5"><g pointer-events="visiblePainted" data-accepts="events"><defs><style>.d-6VARNRm2WT9 { fill: rgba(212, 167, 122, 1) }</style></defs><g id="a" /><g id="b"><g id="c"><path class="d-6VARNRm2WT9" d="M0,3.37C.58,1.42,2.38,0,4.51,0s3.93,1.42,4.51,3.37v5.7c0,.83-.85,1.51-1.89,1.51H1.89c-1.04,0-1.89-.67-1.89-1.51V3.37Z" /></g></g></g></svg>'
+);
